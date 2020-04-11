642071301a0a3cc4a4a550e1650b03ac9b421615
larasn6706174044 <larasn@student.telkomuniversity.ac.id>
2020-04-12 03:32:34 +0700

    msg structure

--- public/index.php
+++ public/index.php
@@ -29,6 +29,43 @@ $app->get('/', function (Request $request, Response $response, $args) {
     $response->getBody()->write("Hello World!");
     return $response;
 });
+
+$url = "https://api.kawalcorona.com/indonesia/";
+
+$client = curl_init($url);
+curl_setopt($client,CURLOPT_RETURNTRANSFER,true);
+$response = curl_exec($client);
+
+$result = json_decode($response);
+
+$natConfirmed = $result[0]->positif;
+
+$natDeaths = $result[0]->meninggal;
+
+$natRecovered = $result[0]->sembuh;
+
+$datetimeString = $result[1]->lastupdate;
+$Last_Update = date("l d F Y, H:i:s", strtotime($datetimeString));
+
+$intConfirmed = getData("https://services1.arcgis.com/0MSEUqKaxRlEPj5g/arcgis/rest/services/Coronavirus_2019_nCoV_Cases/FeatureServer/1/query?f=json&where=1%3D1&returnGeometry=false&spatialRel=esriSpatialRelIntersects&outFields=*&outStatistics=%5B%7B%22statisticType%22%3A%22sum%22%2C%22onStatisticField%22%3A%22Confirmed%22%2C%22outStatisticFieldName%22%3A%22value%22%7D%5D&cacheHint=true");
+
+$intDeaths = getData("https://services1.arcgis.com/0MSEUqKaxRlEPj5g/arcgis/rest/services/Coronavirus_2019_nCoV_Cases/FeatureServer/1/query?f=json&where=1%3D1&returnGeometry=false&spatialRel=esriSpatialRelIntersects&outFields=*&outStatistics=%5B%7B%22statisticType%22%3A%22sum%22%2C%22onStatisticField%22%3A%22Deaths%22%2C%22outStatisticFieldName%22%3A%22value%22%7D%5D&cacheHint=true");
+
+$intRecovered = getData("https://services1.arcgis.com/0MSEUqKaxRlEPj5g/arcgis/rest/services/Coronavirus_2019_nCoV_Cases/FeatureServer/1/query?f=json&where=1%3D1&returnGeometry=false&spatialRel=esriSpatialRelIntersects&outFields=*&outStatistics=%5B%7B%22statisticType%22%3A%22sum%22%2C%22onStatisticField%22%3A%22Recovered%22%2C%22outStatisticFieldName%22%3A%22value%22%7D%5D&cacheHint=true");
+
+$interConfirmed = $intConfirmed->features[0]->attributes->value;
+$interDeaths = $intDeaths->features[0]->attributes->value;
+$interRecovered = $intRecovered->features[0]->attributes->value;
+
+
+function getData($url){
+
+    $client = curl_init($url);
+    curl_setopt($client,CURLOPT_RETURNTRANSFER,true);
+    $response = curl_exec($client);
+ 
+    return json_decode($response);
+ };
  
 // buat route untuk webhook
 $app->post('/webhook', function (Request $request, Response $response) use ($channel_secret, $bot, $httpClient, $pass_signature) {
@@ -201,44 +238,10 @@ $data = json_decode($body, true);
             }
         }
     }
+    
  
 });
 
-$url = "https://api.kawalcorona.com/indonesia/";
 
-$client = curl_init($url);
-curl_setopt($client,CURLOPT_RETURNTRANSFER,true);
-$response = curl_exec($client);
-
-$result = json_decode($response);
-
-$natConfirmed = $result[0]->positif;
-
-$natDeaths = $result[0]->meninggal;
-
-$natRecovered = $result[0]->sembuh;
-
-$datetimeString = $result[1]->lastupdate;
-$Last_Update = date("l d F Y, H:i:s", strtotime($datetimeString));
-
-$intConfirmed = getData("https://services1.arcgis.com/0MSEUqKaxRlEPj5g/arcgis/rest/services/Coronavirus_2019_nCoV_Cases/FeatureServer/1/query?f=json&where=1%3D1&returnGeometry=false&spatialRel=esriSpatialRelIntersects&outFields=*&outStatistics=%5B%7B%22statisticType%22%3A%22sum%22%2C%22onStatisticField%22%3A%22Confirmed%22%2C%22outStatisticFieldName%22%3A%22value%22%7D%5D&cacheHint=true");
-
-$intDeaths = getData("https://services1.arcgis.com/0MSEUqKaxRlEPj5g/arcgis/rest/services/Coronavirus_2019_nCoV_Cases/FeatureServer/1/query?f=json&where=1%3D1&returnGeometry=false&spatialRel=esriSpatialRelIntersects&outFields=*&outStatistics=%5B%7B%22statisticType%22%3A%22sum%22%2C%22onStatisticField%22%3A%22Deaths%22%2C%22outStatisticFieldName%22%3A%22value%22%7D%5D&cacheHint=true");
-
-$intRecovered = getData("https://services1.arcgis.com/0MSEUqKaxRlEPj5g/arcgis/rest/services/Coronavirus_2019_nCoV_Cases/FeatureServer/1/query?f=json&where=1%3D1&returnGeometry=false&spatialRel=esriSpatialRelIntersects&outFields=*&outStatistics=%5B%7B%22statisticType%22%3A%22sum%22%2C%22onStatisticField%22%3A%22Recovered%22%2C%22outStatisticFieldName%22%3A%22value%22%7D%5D&cacheHint=true");
-
-$interConfirmed = $intConfirmed->features[0]->attributes->value;
-$interDeaths = $intDeaths->features[0]->attributes->value;
-$interRecovered = $intRecovered->features[0]->attributes->value;
-
-
-function getData($url){
-
-    $client = curl_init($url);
-    curl_setopt($client,CURLOPT_RETURNTRANSFER,true);
-    $response = curl_exec($client);
- 
-    return json_decode($response);
- };
 
 $app->run();
