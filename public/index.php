e92161acb69290660a48e5b9a0c7b88e21af3786
larasn6706174044 <larasn@student.telkomuniversity.ac.id>
2020-04-12 03:54:06 +0700

    msg structure

--- public/index.php
+++ public/index.php
@@ -110,7 +110,7 @@ $data = json_decode($body, true);
                         $natRecovered = $result[0]->sembuh;
 
                         $datetimeString = $result[1]->Last_Update;
-                        $Last_Update = date(strtotime($datetimeString));
+                        $Last_Update = strtotime($datetimeString);
 
                         // $intConfirmed = getData("https://services1.arcgis.com/0MSEUqKaxRlEPj5g/arcgis/rest/services/Coronavirus_2019_nCoV_Cases/FeatureServer/1/query?f=json&where=1%3D1&returnGeometry=false&spatialRel=esriSpatialRelIntersects&outFields=*&outStatistics=%5B%7B%22statisticType%22%3A%22sum%22%2C%22onStatisticField%22%3A%22Confirmed%22%2C%22outStatisticFieldName%22%3A%22value%22%7D%5D&cacheHint=true");
 
